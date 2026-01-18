5d6e11b5580b76947a558a663ba6e1b491f9bb88
TanKhoa1709 <nguyentankhoa170924@gmail.com>
2026-01-18 17:06:10 +0700

    feat(auth): Implement login page
    - Adds a basic `LoginPage` with email and password fields, and a login button.
    - Implements reusable `MyButton` and `MyTextField` widgets.
    - Defines a `lightMode` theme with a custom color scheme.
    - Sets `LoginPage` as the home widget and applies the new theme.

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,91 @@
+import 'package:chat_app_flutter/components/my_button.dart';
+import 'package:chat_app_flutter/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  LoginPage({super.key});
+
+  void login() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(height: 50),
+
+            // "Welcome back" message
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Email textfield
+            MyTextField(
+              hintText: 'Email',
+              obscureText: false,
+              controller: _emailController,
+            ),
+
+            const SizedBox(height: 10),
+
+            // Password textfield
+            MyTextField(
+              hintText: 'Password',
+              obscureText: true,
+              controller: _passwordController,
+            ),
+
+            const SizedBox(height: 25),
+
+            // Login button
+            MyButton(
+              text: 'Login',
+              onTap: login,
+            ),
+
+            const SizedBox(height: 25),
+
+            // "Register now" button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not a member? ',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                Text(
+                  'Register now!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

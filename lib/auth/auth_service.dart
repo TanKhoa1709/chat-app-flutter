0b4f25120fd41aa5b1a56e731c27a583f050e7ea
TanKhoa1709 <nguyentankhoa170924@gmail.com>
2026-01-19 22:49:19 +0700

    feat(auth): Integrate Firebase Authentication
    - Initializes Firebase in `main.dart`.
    - Adds `firebase_core` and `firebase_auth` as dependencies.
    - Implements an `AuthService` class
    - Includes Firebase configuration files for Android and iOS

---
+++ lib/auth/auth_service.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  // Instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in
+  Future<UserCredential> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:chat_app_flutter/auth/login_or_register.dart';
+import 'package:chat_app_flutter/firebase_options.dart';
 import 'package:chat_app_flutter/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 

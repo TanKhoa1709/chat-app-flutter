9bdcd2b678cad53b2046b2d057056ef562b1b25d
TanKhoa1709 <nguyentankhoa170924@gmail.com>
2026-01-20 17:08:08 +0700

    feat(auth): Add navigation drawer and settings page
    - Creates a `MyDrawer` widget with navigation for Home and Settings.
    - Adds a `SettingPage` with a basic `Scaffold` and `AppBar`.
    - Integrates `MyDrawer` into the `HomePage`, replacing the previous logout button.
    - Moves the logout functionality into the drawer.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,16 @@
-import 'package:chat_app_flutter/auth/auth_service.dart';
+import 'package:chat_app_flutter/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() {
-    final AuthService authService = AuthService();
-    authService.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-        actions: [
-          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-        ],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
